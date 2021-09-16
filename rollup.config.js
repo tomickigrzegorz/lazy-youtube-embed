56daa8debe92f3b6736c82818bb97221c0113c50
Grzegorz Tomicki <gtomik@gmail.com>
2021-09-16 10:40:15 +0200

    feat: js converted into ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,57 @@
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import babel from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const { PRODUCTION } = process.env;
+const extensions = ['.ts'];
 
 const plugins = ({ module }) => {
   return [
     babel({
+      babelrc: false,
       exclude: 'node_modules/**',
       babelHelpers: 'bundled',
+      presets: ['@babel/preset-env'],
+      plugins: ['@babel/proposal-class-properties'],
+      extensions,
     }),
-    PRODUCTION && terser({
-      module,
-      mangle: true,
-      compress: true,
+    PRODUCTION &&
+      terser({
+        module,
+        mangle: true,
+        compress: true,
+      }),
+    typescript({
+      noEmitOnError: false,
     }),
     !PRODUCTION && serve({ open: true, contentBase: 'docs' }),
     !PRODUCTION && livereload(),
-  ]
-}
+  ];
+};
 
 const configs = [
   {
-    input: 'sources/script.js',
+    input: 'sources/script.ts',
     output: {
       format: 'iife',
       file: pkg.main,
-      name: 'ytLazy'
+      name: 'ytLazy',
     },
     plugins: plugins({ module: false }),
   },
   {
-    input: 'sources/script.js',
+    input: 'sources/script.ts',
     watch: false,
     output: {
       format: 'umd',
       file: pkg.browser,
-      name: 'ytLazy'
+      name: 'ytLazy',
     },
     plugins: plugins({ module: true }),
-  }
+  },
 ];
 
 export default configs;
