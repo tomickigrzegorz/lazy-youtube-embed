299795aa516d368927a94f674cd08b281770523d
Grzegorz Tomicki <gtomik@gmail.com>
2021-12-21 00:40:35 +0100

    build: new dist folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,134 @@
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
-import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
+import cleanup from 'rollup-plugin-cleanup';
+import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+
 import pkg from './package.json';
 
 const { PRODUCTION } = process.env;
-const extensions = ['.ts'];
+const input = 'sources/js/index.ts';
 
-const plugins = ({ module }) => {
-  return [
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env'],
-      plugins: ['@babel/proposal-class-properties'],
-      extensions,
-    }),
-    PRODUCTION &&
-      terser({
-        module,
-        mangle: true,
-        compress: true,
+export default [
+  {
+    input,
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      typescript({
+        noEmitOnError: false,
       }),
-    typescript({
-      noEmitOnError: false,
-    }),
-    !PRODUCTION && serve({ open: true, contentBase: 'docs' }),
-    !PRODUCTION && livereload(),
-  ];
-};
-
-const configs = [
-  {
-    input: 'sources/script.ts',
-    output: {
-      format: 'iife',
-      file: pkg.main,
-      name: 'ytLazy',
-    },
-    plugins: plugins({ module: false }),
-  },
-  {
-    input: 'sources/script.ts',
+      cleanup(),
+    ],
     watch: false,
     output: {
-      format: 'umd',
-      file: pkg.browser,
       name: 'ytLazy',
+      format: 'iife',
+      file: pkg.main,
+      sourcemap: true,
     },
-    plugins: plugins({ module: true }),
+  },
+  {
+    input,
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      typescript({
+        noEmitOnError: false,
+      }),
+      cleanup(),
+    ],
+    watch: false,
+    output: {
+      name: 'ytLazy',
+      format: 'iife',
+      sourcemap: false,
+      file: 'dist/js/youtubeLazy.min.js',
+      plugins: [terser()],
+    },
+  },
+  {
+    input,
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      typescript({
+        noEmitOnError: false,
+      }),
+      cleanup(),
+    ],
+    output: {
+      name: 'ytLazy',
+      format: 'iife',
+      sourcemap: true,
+      file: 'docs/js/youtubeLazy.min.js',
+      plugins: [
+        terser({
+          mangle: true,
+        }),
+        !PRODUCTION && serve({ open: true, contentBase: ['docs'] }),
+        !PRODUCTION && livereload(),
+      ],
+    },
+  },
+  {
+    input,
+    watch: false,
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      typescript({
+        noEmitOnError: false,
+      }),
+      cleanup(),
+    ],
+    output: [
+      {
+        name: 'ytLazy',
+        format: 'umd',
+        sourcemap: true,
+        file: 'dist/js/youtubeLazy.umd.js',
+      },
+      {
+        name: 'ytLazy',
+        format: 'umd',
+        sourcemap: false,
+        file: 'dist/js/youtubeLazy.umd.min.js',
+        plugins: [
+          terser({
+            mangle: true,
+            compress: { drop_console: true, drop_debugger: true },
+          }),
+        ],
+      },
+    ],
+  },
+  {
+    input,
+    watch: false,
+    plugins: [
+      babel({ babelHelpers: 'bundled' }),
+      typescript({
+        noEmitOnError: false,
+      }),
+      cleanup(),
+    ],
+    output: [
+      {
+        name: 'ytLazy',
+        format: 'es',
+        sourcemap: true,
+        file: 'dist/js/youtubeLazy.esm.js',
+      },
+      {
+        name: 'ytLazy',
+        format: 'es',
+        sourcemap: false,
+        file: 'dist/js/youtubeLazy.esm.min.js',
+        plugins: [
+          terser({
+            mangle: true,
+            compress: { drop_console: true, drop_debugger: true },
+          }),
+        ],
+      },
+    ],
   },
 ];
-
-export default configs;
