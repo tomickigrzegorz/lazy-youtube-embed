3e303d30438e96e058ef0db4a17c2b18e8ec41e9
Grzegorz Tomicki <gtomik@gmail.com>
2022-03-07 11:03:34 +0100

    feat: new prop picture

--- dist/js/youtubeLazy.umd.js
+++ dist/js/youtubeLazy.umd.js
@@ -33,12 +33,12 @@
     };
 
     class ytLazy {
-        constructor(classElement, { background = "rgba(0,0,0,0.9)", maxWidth = 90, overflow = false, createWatchIn = () => { }, }) {
+        constructor(classElement, { background = "rgba(0,0,0,0.9)", maxWidth = 90, overflow = false, picture = false, createWatchIn = () => { }, }) {
             this._initial = () => {
                 const getYTLazy = document.querySelectorAll(`.${this._class}`);
                 for (let i = 0; i < getYTLazy.length; i++) {
-                    const { id, openIn, title } = parseJson(getYTLazy[i].getAttribute("data-yt"));
-                    getYTLazy[i].appendChild(this._createImage(id));
+                    const { id, openIn, title, picture } = parseJson(getYTLazy[i].getAttribute("data-yt"));
+                    getYTLazy[i].appendChild(this._createImage(id, picture));
                     getYTLazy[i].appendChild(createRedButton());
                     if (title !== null && title !== void 0 ? title : false) {
                         const titleElement = `<div class="ytLazy__title">${title}</div><div class="ytLazy__gradient-top"></div>`;
@@ -55,12 +55,43 @@
                 }
                 this._handEvent();
             };
-            this._createImage = (id) => {
+            this._createImage = (id, pic) => {
+                const sourcesArray = [
+                    {
+                        media: "(min-width: 1440px)",
+                        srcset: this._sourceURL(id, "maxresdefault"),
+                    },
+                    {
+                        media: "(min-width: 1024px)",
+                        srcset: this._sourceURL(id, "hqdefault"),
+                    },
+                    {
+                        media: "(min-width: 480px)",
+                        srcset: this._sourceURL(id, "mqdefault"),
+                    },
+                ];
+                const picture = this._createElement("picture");
+                sourcesArray.map((element) => {
+                    picture.appendChild(this._createElement("source", element));
+                });
                 const image = new Image();
                 image.className = "ytLazy__image";
                 image.loading = "lazy";
-                image.src = `//i.ytimg.com/vi/${id}/sddefault.jpg`;
-                return image;
+                image.width = 1050;
+                image.height = 787;
+                image.src = this._sourceURL(id, "sddefault");
+                picture.appendChild(image);
+                return this._picture || pic ? picture : image;
+            };
+            this._sourceURL = (id, type) => {
+                return `//i.ytimg.com/vi/${id}/${type}.jpg`;
+            };
+            this._createElement = (el, config) => {
+                const element = document.createElement(el);
+                if (config) {
+                    setAttribute(element, config);
+                }
+                return element;
             };
             this._setLightbox = (target) => {
                 const watchIn = target.closest(".ytLazy__watch-in-link");
@@ -135,6 +166,7 @@
             this._class = classElement;
             this._background = background;
             this._overflow = overflow;
+            this._picture = picture;
             this._maxWidth = maxWidth;
             this._createWatchIn = createWatchIn;
             this._link = "https://www.youtube.com";
