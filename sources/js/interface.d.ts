76a839b55685b53f598a975cd637a93811cb15bd
Grzegorz Tomicki <gtomik@gmail.com>
2022-03-08 12:16:31 +0100

    feat: add local to global config

--- sources/js/interface.d.ts
+++ sources/js/interface.d.ts
@@ -1,13 +1,22 @@
 interface ConstructorObject {
   background?: string;
+  maxWidth?: number;
   overflow?: boolean;
+  local?: boolean;
   picture?: boolean;
-  maxWidth?: number | null;
   createWatchIn?: () => void;
-  open?: boolean;
 }
 
 interface ConfigObject {
   media: string;
   srcset: string;
+}
+
+interface LooseObject {
+  [key: string]: any;
+}
+
+interface lightboxObject {
+  id: string;
+  maxWidth: string;
 }
